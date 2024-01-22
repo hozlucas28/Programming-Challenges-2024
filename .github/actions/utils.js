f7881df9b79a5ff235c87e3119cb657efbde54fb
hozlucas28 <hozlucas28@gmail.com>
2024-01-22 04:54:58 -0300

    Refactor utils.js to add new functions and improve existing ones

--- .github/actions/utils.js
+++ .github/actions/utils.js
@@ -2,12 +2,30 @@ const data = require('./data.json')
 const stats = require('../../Roadmap/stats.json')
 
 const challenges = {
-	getFolderNames: () => stats.challenges_ranking.map(({ name }) => name),
+	getChallengeProps: (/** @type {string} */ challengeNumber) => {
+		const challengesProps = challenges.getChallengesProps()
+		return challengesProps.find(({ number }) => number === challengeNumber)
+	},
+	getChallengesProps: () =>
+		stats.challenges_ranking.map(({ name }) => {
+			const separatorIndex = name.indexOf('-')
+
+			return {
+				folderName: name.trim(),
+				number: name.slice(0, separatorIndex).trim(),
+				title: name.slice(separatorIndex + 1).trim(),
+			}
+		}),
+	getNumbers: () => challenges.getChallengesProps().map(({ number }) => number),
 }
 
 const programmingLanguages = {
 	getExtensionNames: () => data.languages.map(({ fileExtensionName }) => fileExtensionName),
 	getFolderNames: () => data.languages.map(({ folderName }) => folderName),
+	getLanguageProps: (/** @type {string} */ programmingLanguageName) => {
+		const language = data.languages.find(({ names }) => names.includes(programmingLanguageName))
+		return language
+	},
 	getNames: () => data.languages.map(({ names }) => names).flat(),
 }
 
