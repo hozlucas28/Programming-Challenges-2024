13c6263a7fe4799b16ef6413e7353a4a15e792b3
hozlucas28 <hozlucas28@gmail.com>
2024-01-22 14:35:26 -0300

    Refactor utils.js file

--- .github/actions/utils.js
+++ .github/actions/utils.js
@@ -1,27 +1,19 @@
-const data = require('./data.json')
-const stats = require('../../Roadmap/stats.json')
+const fs = require('node:fs')
+const path = require('node:path')
 
-const challenges = {
-	getNumbers: () => challenges.getChallengesProps().map(({ number }) => number),
+const roadmapPath = path.resolve('../../Roadmap')
+const excludeExtensions = ['.md', '.json']
 
-	getChallengeProps: (challengeNumber) => {
-		const challengesProps = challenges.getChallengesProps()
-		return challengesProps.find(({ number }) => number === challengeNumber)
-	},
-
-	getChallengesProps: () =>
-		stats.challenges_ranking.map(({ name }) => {
-			const separatorIndex = name.indexOf('-')
-
-			return {
-				folderName: name.trim(),
-				number: name.slice(0, separatorIndex).trim(),
-				title: name.slice(separatorIndex + 1).trim(),
-			}
-		}),
-}
-
-function formatArraytoString({ array, finalSeparator, separator }) {
+// Helpers
+/**
+ * Converts an array of strings to a string with specified separators.
+ * @param {Object} options - The options object.
+ * @param {string[]} options.array - The array of strings to convert.
+ * @param {string} options.finalSeparator - The separator to use before the last element.
+ * @param {string} options.separator - The separator to use between elements.
+ * @returns {string} The converted string.
+ */
+function arraytoString({ array, finalSeparator, separator }) {
 	const lastElementI = array.length - 1
 
 	const stringifiedArray = array.reduce((prev, current, index) => {
@@ -35,26 +27,40 @@ function formatArraytoString({ array, finalSeparator, separator }) {
 	return stringifiedArray
 }
 
-const programmingLanguages = {
-	getExtensionNames: () => data.languages.map(({ fileExtension }) => fileExtension),
-	getFolderNames: () => stats.languages_ranking.map(({ name }) => name),
-	getNames: () => data.languages.map(({ name }) => name).flat(),
+// Challenges data
+function getChallengeFolders() {
+	const roadmapContent = fs.readdirSync(roadmapPath, { encoding: 'utf-8', withFileTypes: true })
+	const challengeFolders = roadmapContent.filter((dirent) => dirent.isDirectory())
+	return challengeFolders.map((folder) => folder.name)
+}
 
-	getLanguageProps: (programmingLanguage) => {
-		const language = data.languages.find(({ name }) => name === programmingLanguage)
-		const folderName = stats.languages_ranking.find(
-			({ name }) => name.toLowerCase() === programmingLanguage.toLowerCase()
-		)?.name
+function getChallengeNumbers() {
+	const challengeFolders = getChallengeFolders()
+	const challengeNumbers = challengeFolders.map((folder) => folder.slice(0, 2))
+	return challengeNumbers
+}
 
-		return {
-			...language,
-			folderName,
-		}
-	},
+// Programming languages data
+function getProgrammingLanguageExtensions() {
+	const roadmapContentDeep = fs.readdirSync(roadmapPath, { recursive: true, withFileTypes: true })
+	const files = roadmapContentDeep.filter((file) => file.isFile())
+	const fileExtensions = files.map((file) => path.extname(file.name))
+	const fileExtensionsFiltered = fileExtensions.filter((extension) => !excludeExtensions.includes(extension))
+	return [...new Set(fileExtensionsFiltered).values()]
+}
+
+function getProgrammingLanguageFolderNames() {
+	const challengesFolders = getChallengeFolders()
+	const foldersToSeek = challengesFolders.map((folder) => path.resolve(`${roadmapPath}/${folder}`))
+	const foldersContent = foldersToSeek.map((folder) => fs.readdirSync(folder)).flat()
+	const folderNames = foldersContent.filter((folder) => path.extname(folder) === '')
+	return [...new Set(folderNames).values()]
 }
 
 module.exports = {
-	challenges,
-	formatArraytoString,
-	programmingLanguages,
+	arraytoString,
+	getChallengeFolders,
+	getChallengeNumbers,
+	getProgrammingLanguageFolderNames,
+	getProgrammingLanguageExtensions,
 }
