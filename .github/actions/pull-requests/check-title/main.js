d70552a1528370fc889abb5199af342bf7fc2f5f
hozlucas28 <hozlucas28@gmail.com>
2024-01-22 02:34:16 -0300

    Add outputs for challenge number and programming language name based on pull request title

--- .github/actions/pull-requests/check-title/main.js
+++ .github/actions/pull-requests/check-title/main.js
@@ -1,18 +1,24 @@
 const core = require('@actions/core')
-const utils = require('../utils.js')
+const utils = require('../../utils.js')
 
 // Required data
 const title = core.getInput('title')
 const programmingLanguageNames = utils.programmingLanguages.getNames()
 
 // Check if the title is valid
-const regex = new RegExp(`#[0-9][0-9] - (${programmingLanguageNames.join('|')})`)
-const isValidTitle = (title.match(regex)?.length ?? 0) > 0
+const regex = new RegExp(`#([0-9][0-9]) - (${programmingLanguageNames.join('|')})`)
+const matches = title.match(regex)
+const isValidTitle = matches && matches.length > 0
 
-if (!isValidTitle)
+if (isValidTitle) {
+	// On valid title, set the outputs
+	core.setOutput('challenge-number', matches[1])
+	core.setOutput('programming-language-name', matches[2])
+} else {
 	// On invalid title, set the action as failed
 	core.setFailed(
 		'Invalid pull request title. ' +
 			'Should follow this format: #<CHALLENGE NUMBER> - <LANGUAGE NAME>. ' +
 			'For example: #01 - JavaScript'
 	)
+}
