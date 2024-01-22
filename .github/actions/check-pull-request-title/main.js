a6cea98e6522565a8892db9633e038a9fe277338
hozlucas28 <hozlucas28@gmail.com>
2024-01-21 23:41:40 -0300

    Update comments

--- .github/actions/check-pull-request-title/main.js
+++ .github/actions/check-pull-request-title/main.js
@@ -5,12 +5,12 @@ const utils = require('../utils.js')
 const title = core.getInput('title')
 const programmingLanguageNames = utils.programmingLanguages.getNames()
 
-// Check if the pull request title is valid
+// Check if the title is valid
 const regex = new RegExp(`#[0-9][0-9] - (${programmingLanguageNames.join('|')})`)
 const isValidTitle = (title.match(regex)?.length ?? 0) > 0
 
 if (!isValidTitle)
-	// On invalid pull request title, set the action as failed
+	// On invalid title, set the action as failed
 	core.setFailed(
 		'Invalid pull request title. Should follow this format: #<CHALLENGE NUMBER> - <LANGUAGE NAME>. For example: #01 - JavaScript '
 	)
