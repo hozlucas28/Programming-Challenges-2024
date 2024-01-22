7167b8599623f8d30b37e97dc6efe6173753fd69
hozlucas28 <hozlucas28@gmail.com>
2024-01-22 04:49:23 -0300

    Refactor pull request title validation logic

--- .github/actions/pull-requests/check-title/main.js
+++ .github/actions/pull-requests/check-title/main.js
@@ -1,24 +1,65 @@
 const core = require('@actions/core')
 const utils = require('../../utils.js')
+const process = require('node:process')
 
 // Required data
 const title = core.getInput('title')
+const challengeNumbers = utils.challenges.getNumbers()
 const programmingLanguageNames = utils.programmingLanguages.getNames()
 
-// Check if the title is valid
-const regex = new RegExp(`#([0-9][0-9]) - (${programmingLanguageNames.join('|')})`)
-const matches = title.match(regex)
-const isValidTitle = matches && matches.length > 0
+// Regular expressions
+const challengeNumberRegex = new RegExp(`#(${challengeNumbers.join('|')})`)
+const programmingLanguageNameRegex = new RegExp(`(${programmingLanguageNames.join('|')})+$`)
+const titleRegex = new RegExp(`#(${challengeNumbers.join('|')}) - (${programmingLanguageNames.join('|')})`)
 
-if (isValidTitle) {
-	// On valid title, set the outputs
-	core.setOutput('challenge-number', matches[1])
-	core.setOutput('programming-language-name', matches[2])
-} else {
-	// On invalid title, set the action as failed
+// Check if challenge number is valid
+const isValidChallengeNumber = challengeNumberRegex.test(title)
+if (!isValidChallengeNumber) {
+	const availableChallengeNumbers = challengeNumbers.reduce((prev, current, index, array) => {
+		if (index === 0) return current.toString()
+		if (index === array.length - 1) return prev + ', or ' + current
+		return prev + ', ' + current
+	})
+
 	core.setFailed(
-		'Invalid pull request title. ' +
-			'Should follow this format: #<CHALLENGE NUMBER> - <LANGUAGE NAME>. ' +
-			'For example: #01 - JavaScript'
+		"Challenge number of the pull request title doesn't match with existing ones. " +
+			'Please check the challenge number of the pull request title. ' +
+			`It should be one of these: ${availableChallengeNumbers}` +
+			'If you think this is an error, please contact an administrator.'
 	)
+}
+
+// Check if programming language name is valid
+const isValidProgrammingLanguageName = programmingLanguageNameRegex.test(title)
+if (!isValidProgrammingLanguageName) {
+	const availableProgrammingLanguageNames = programmingLanguageNames.reduce((prev, current, index, array) => {
+		if (index === 0) return current
+		if (index === array.length - 1) return prev + ', or ' + current
+		return prev + ', ' + current
+	})
+
+	core.setFailed(
+		"Programming language name of the pull request title doesn't match with existing ones. " +
+			'Please check the programming language name of the pull request title. ' +
+			`It should be one of these: ${availableProgrammingLanguageNames}` +
+			'If you think this is an error, please contact an administrator.'
+	)
+}
+
+if (isValidChallengeNumber && isValidProgrammingLanguageName) {
+	const matches = title.match(titleRegex)
+	const isValidTitle = matches && matches.length > 0
+
+	if (isValidTitle) {
+		// On valid title, set the outputs
+		core.setOutput('challenge-number', matches[1])
+		core.setOutput('programming-language-name', matches[2])
+	} else {
+		// On invalid title, set the action as failed
+		core.setFailed(
+			'Invalid pull request title format. ' +
+				'It should be: #<CHALLENGE NUMBER> - <LANGUAGE NAME>. ' +
+				'For example: #01 - JavaScript'
+		)
+	}
 }
